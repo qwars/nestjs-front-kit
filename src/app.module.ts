e2b4ed4315053c332c2edf6f4d440abfa87b7bc2
Alexandr Selunin <qwars@SystemVM.qwars.loc>
2024-08-17 04:24:01 +0500

    Среда разработки для React

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import {join} from 'path';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '../..', 'public') }),
   ],
   controllers: [AppController],
   providers: [AppService],
